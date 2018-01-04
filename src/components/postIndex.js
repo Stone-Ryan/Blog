a6b3f8aa667a2711e92426ccccd9a67b91117117
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-03 19:01:05 -0800

    started PostIndex component and removed App component from project

---
+++ src/components/postIndex.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostIndex extends Component {
+  render() {
+    return (
+      <div>
+        PostIndex
+      </div>
+    );
+  }
+}
+
+export default PostIndex;

5b2a6b0b097541f3e7808363ff85fb52e7083cc8
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-05 14:23:05 -0800

    wrote out NewPost component with validation

--- src/components/newPosts.js
+++ src/components/newPosts.js
@@ -1,9 +1,73 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class NewPost extends Component {
+  renderField(field) {
+    const { touched, error } = field.meta
+
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+         />
+         {touched && error}
+      </div>
+    )
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render(){
-    return <div>NewPost</div>;
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          name="title"
+          component={this.renderField}
+          label="Title"
+        />
+        <Field
+          name="catagories"
+          component={this.renderField}
+          label="Catagories"
+        />
+        <Field
+          name="content"
+          component={this.renderField}
+          label="Post Content"
+        />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
   }
 }
 
-export default NewPost;
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Enter a title!"
+  }
+  if (!values.catagories) {
+    errors.catagories = "Enter catagories!"
+  }
+  if (!values.content) {
+    errors.content = "Enter content!"
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'NewPostForm'
+})(NewPost);

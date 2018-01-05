fc03cac99d5a42e845dc00edc7da648f7ca07e1d
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-04 20:28:38 -0800

    added ;

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import reducers from './reducers';
 import PostIndex from './components/postIndex';

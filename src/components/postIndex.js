a4caba37cf8f6a0103924b498acce88ea73bc5dd
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-04 21:17:24 -0800

    added renderPosts method to PostIndex

--- src/components/postIndex.js
+++ src/components/postIndex.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { fetchPosts } from '../actions'
 
 
 class PostIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return <li className="list-group-item" key={post.title}>
+        {post.title}
+      </li>
+    })
+  }
+
   render() {
     return (
       <div>
-        PostIndex
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostIndex);
+const mapStateToProps = state => ({
+  posts: state.posts
+})
+
+export default connect(mapStateToProps, { fetchPosts })(PostIndex);
